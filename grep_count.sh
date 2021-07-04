6e0af3e3af92afe1c252a00f4d2e943fe3ad0bca
User_HP <User_HP@git.tst>
2021-07-04 15:17:17 +0200

    grep story: add command to search in all csv files for a pattern

--- grep_count.sh
+++ grep_count.sh
@@ -1,19 +1,26 @@
 #! /bin/bash
 
-echo "Hellooo"
-
 awk -F',' '/Liverpool/           {print " *** Nationality: "$11}' england_league_players.csv > Liverpool.txt
 awk -F',' '/Arsenal/             {print " *** Nationality: "$11}' england_league_players.csv > Arsenal.txt
 awk -F',' '/Chelsea/             {print " *** Nationality: "$11}' england_league_players.csv > Chelsea.txt
 awk -F',' '/Manchester United/   {print " *** Nationality: "$11}' england_league_players.csv > ManUnited.txt
 
+# print out how many Defender in all csv files
+grep -rc 'Defender' --include *.csv
+
+# search for string matching the pattern [De*der] in any csv file
+grep -r -o '\bDe\w*der\b' --include *.csv
+# -r to search recursively
+# -o to output what matches the pattern
+# \b delimiter for beginning of pattern
+# \w* to include any character between 'De' and 'der'
+# \b delimiter for end of pattern
 
 while read x; do 
     retVal=$(grep "${x}" Arsenal.txt Chelsea.txt ManUnited.txt)
 	if [ $? -eq 1 ]; then
 	    echo "${x}" >> in_Liverpool_only.grep
 	fi
-	
 	if [ $(echo "${retVal}" | wc -l) -ge 1 ]; then
 	    echo "${retVal}" >> in_others.grep
 	fi	
